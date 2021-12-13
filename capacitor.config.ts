6e8e94eac690293863eb9f8fba1591ceab15c00a
Robin Zweifel <robin.zweifel@gmail.com>
2021-12-13 09:43:24 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Travel Log',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
