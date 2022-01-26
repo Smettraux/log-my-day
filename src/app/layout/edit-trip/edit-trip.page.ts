06ce597ad2ddcffa36a8bae726830b2efe12d26e
Nathan Zweifel <nathan.zweifel@heig-vd.ch>
2022-01-26 17:02:45 +0100

    style: edit trip page

--- src/app/layout/edit-trip/edit-trip.page.ts
+++ src/app/layout/edit-trip/edit-trip.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/auth/auth.service";
+import { TripService } from "src/app/services/trip.service";
+import { environment } from "src/environments/environment";
+import { Trip, TripToAdd } from "src/app/models/trip";
 
 @Component({
   selector: 'app-edit-trip',
@@ -6,10 +11,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-trip.page.scss'],
 })
 export class EditTripPage implements OnInit {
+  title: string;
+  description: string;
 
-  constructor() { }
+  constructor(
+    // Inject the authentication provider.
+    private auth: AuthService,
+    // Inject the router
+    private router: Router,
+    private tripService: TripService
+  ) { }
 
   ngOnInit() {
+  }
+
+  // Add a method to log out.
+  logOut() {
+    console.log("logging out...");
+    this.auth.logOut();
+    this.router.navigateByUrl("/login");
+  }
+
+  editTrip():void {
+    let tripToAdd:TripToAdd = {
+      "title": this.title,
+      "description": this.description
+    }
+    this.tripService.addTrip(tripToAdd).subscribe(trip => {
+      this.router.navigate(['/trip-list'], { state: { show: "true" } });
+      this.title=this.description="";
+    }, err => {
+      console.warn("Impossible to add", err);
+    });
   }
 
 }
