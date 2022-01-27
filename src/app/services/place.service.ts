5e9e111625a0bac854c94cd470dc5c655d1f7483
Steve Mettraux <steve.mettraux@heig-vd.ch>
2022-01-27 17:57:06 +0100

    -feat: marker is added on map on new location

--- src/app/services/place.service.ts
+++ src/app/services/place.service.ts
@@ -13,7 +13,51 @@ export class PlaceService {
 
   constructor(private http: HttpClient) { }
 
+  getPlaces(search: string ="") : Observable<Place[]> {
+    const url = `${API_URL}/places` ;
+    return this.http
+    .get<PlaceResponse[]>(url)
+    .pipe(map(this.convertPlaceResponseToPlace));
+  }
+
+  getPlace(id: string): Observable<Place> {
+    const url = `${API_URL}/places?id=${id}`;
+    return this.http
+    .get<PlaceResponse>(url)
+    .pipe(map((place) => {
+      let placeToReturn: Place = {
+        id: place[0].id,
+        href: place[0].href,
+        name: place[0].name,
+        description: place[0].description,
+        location: place[0].location,
+        pictureUrl: place[0].pictureUrl,
+        createdAt: place[0].createdAt,
+      }
+      return placeToReturn;
+    }
+    ));
+  }
+
   addPlace(place: PlaceToAdd): Observable<Place> {
     return this.http.post<Place>(`${API_URL}/places`, place);
+  }
+
+  convertPlaceResponseToPlace(response: PlaceResponse[]): Place[] {
+    let places:Place[] = [];
+    response.forEach((place: Place) => {
+      places.push(
+        {
+          id: place.id,
+          href: place.href,
+          name: place.name,
+          description: place.description,
+          location: place.location,
+          pictureUrl: place.pictureUrl,
+          createdAt: place.createdAt,
+        }
+      );
+    });
+    return places;
   }
 }
